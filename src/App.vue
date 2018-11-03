54e4ca7ed0348dacc81b443ab02cb2b5c7fdc897
Wladimir Hernández <elwlaaa@gmail.com>
2018-11-03 19:16:35 -0400

    Sabado

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="contai">
+      <movies-app></movies-app>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MoviesApp from './components/UsersApp';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    MoviesApp
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="row">
+        <div class="col">
+            <div class="form-group mb-2">
+                <label class="sr-only">Escribe Tu Busqueda</label>
+                <input type="text" v-model="search" class="form-control">
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input type="radio" v-model="online" :value="true" class="form-radio-input">
+                Online
+            </div>
+            <div class="form-check form-check-inline">
+                <input type="radio" v-model="online" :value="false" class="form-radio-input">
+                Offline
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    computed:{
+        search:{
+            get(){
+                return this.$store.state.filter.query;
+            },
+            set(val){
+                this.$store.commit('SET_QUERY', val);
+            }
+        },
+        online:{
+            get(){
+                return this.$store.state.filter.online;
+            },
+            set(){
+                this.$store.commit('SET_ONLINE');
+            }
+        },
+    }
+}
+</script>
+
